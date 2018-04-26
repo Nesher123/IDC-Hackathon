f3da14fa5fff6c7ca9baf45dd081af07fcc2e494
YonathanRon <jonron105@gmail.com>
2018-04-26 16:15:33 +0300

    Update UserVideos.java

--- UserVideos.java
+++ UserVideos.java
@@ -6,13 +6,13 @@ import java.util.Date;
 import java.util.ArrayList;
 import java.util.Collections;
 
-
 public class UserVideos {
-	/* 1. open directory
+	/*
+	 * 1. open directory 
 	 * 2. go over all videos inside the directory. 
-	 * 3. For each video, get its time-stamp
-	 * 	3.1. upload to WSC server
-	 * 	3.2. get ID
+	 * 3. For each video, get its time-stamp 
+	 * 	3.1. upload to WSC server 
+	 * 	3.2. get ID 
 	 * 4. return a sorted (by time-stamps) list containing all videos IDs AND time-stamps
 	 * 
 	 * ***** Bdikot!!!!!!
@@ -20,31 +20,30 @@ public class UserVideos {
 
 	static String m_SystemType = "Eurobasket";
 
-	public static ArrayList<Event> openDirectory(String folderPath){
+	public static ArrayList<Event> openDirectory(String folderPath) {
 		// list files
 		File directory = new File(folderPath);
-		File[] contents = directory.listFiles();	
+		File[] contents = directory.listFiles();
 
-		ArrayList<Event> idsAndTimestampsOfUserVideos =  new ArrayList<Event>();
+		ArrayList<Event> idsAndTimestampsOfUserVideos = new ArrayList<Event>();
 
 		for (File f : contents) {
 			// upload the video
+			UploadVideoPost(f.getName(), f, m_SystemType);
 		}
 
 		ArrayList<Event> allUploadedClips = GetUploadedClips(m_SystemType);
 
 		for (Event e : allUploadedClips) {
 			// release the time-stamps
-			for(File f : contents)
-			{
-				if(f.getName().equals(e.))
+			for (File f : contents) {
+				if (f.getName().equals(e.name)) {
 					Date videoCreationTime = releaseTimeStamps(f.getName());
-				// request the id from the system according to the name.
-				int id = e.ID;
-
-				idsAndTimestampsOfUserVideos.add(new Event(id, 0, 0 ,videoCreationTime));
+					// request the id from the system according to the name.
+					int id = e.ID;
+					idsAndTimestampsOfUserVideos.add(new Event(id, 0, 0, videoCreationTime, e.name));
+				}
 			}
-
 		}
 
 		Collections.sort(idsAndTimestampsOfUserVideos);
@@ -53,7 +52,6 @@ public class UserVideos {
 	}
 
 	public static Date releaseTimeStamps(String videoFilePath) throws IOException {
-
 		File videoFile = new File(videoFilePath);
 		if (!videoFile.exists()) {
 			throw new FileNotFoundException("File " + videoFilePath + " not exists");
@@ -70,7 +68,7 @@ public class UserVideos {
 		return creationTime;
 	}
 
-	public static void main(String[] args) {		
+	public static void main(String[] args) {
 		openDirectory("C:\\Users\\neshe\\Desktop\\test");
 	}
 }
